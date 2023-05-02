40f507cd26c82a54ab9f3fc614f239e9943e46ee
Spearton <104092856+Spearton-CS@users.noreply.github.com>
2023-05-02 22:09:13 +0200

    Add files via upload

---
+++ YandexMusicRPC/Detector.cs
@@ -0,0 +1,126 @@
+﻿using Windows.Media.Control;
+
+namespace YandexMusicRPC
+{
+    public delegate void UpdateContent(string? track, string? artist, bool error);
+#pragma warning disable CA1416
+    public class Detector : IDisposable
+    {
+        private GlobalSystemMediaTransportControlsSessionManager GSMTCSM;
+        private GlobalSystemMediaTransportControlsSessionMediaProperties? MediaProperties;
+        private UpdateContent updateContent;
+        private bool Disposed = false;
+        public bool Enabled { get; set; } = true;
+        public TrackInfo TrackInfo { get; private set; } = new();
+        public Thread Thread { get; private set; }
+#pragma warning disable CS8618
+        public Detector(UpdateContent updateContent)
+#pragma warning restore CS8618
+        {
+            this.updateContent = updateContent;
+            Thread = new(StartMusic);
+            Thread.Start();
+        }
+        private async void StartMusic()
+        {
+            GetGSMT();
+            while (!Disposed)
+            {
+                if (Enabled)
+                    try
+                    {
+                        await UpdateContent();
+                    }
+                    catch
+                    {
+
+                    }
+                Thread.Sleep(1000);
+            }
+        }
+        private async void GetGSMT() => GSMTCSM = await GetSystemMediaTransportControlsSessionManager();
+        private async Task UpdateContent()
+        {
+            await Task.Run(async () => {
+                using (null)
+                {
+                    try
+                    {
+                        MediaProperties = await GetMediaProperties(GSMTCSM.GetCurrentSession());
+                    }
+                    catch
+                    {
+                        updateContent(null, null, true);
+                        GC.Collect();
+                        GSMTCSM = await GetSystemMediaTransportControlsSessionManager();
+                        return;
+                    }
+                    if (GSMTCSM is null)
+                        return;
+                    var CurrSession = GSMTCSM.GetCurrentSession();
+                    TrackInfo trackInfo = new(MediaProperties.Title, MediaProperties.Artist);
+                    if (trackInfo.ToString() == TrackInfo.ToString())
+                        return;
+                    TrackInfo = new(MediaProperties.Title, MediaProperties.Artist);
+                    updateContent(MediaProperties.Title, MediaProperties.Artist, false);
+                    GetImage();
+                    MediaProperties = null;
+                    GC.Collect();
+                }
+            });
+        }
+        private async void GetImage()
+        {
+            try
+            {
+                var ssss = await MediaProperties?.Thumbnail.OpenReadAsync();
+                using (StreamReader sr = new(ssss.AsStreamForRead()))
+                {
+                    var bytes = default(byte[]);
+                    using (MemoryStream memstream = new())
+                    {
+                        byte[] buffer = new byte[512];
+                        int bytesRead = default;
+                        while ((bytesRead = sr.BaseStream.Read(buffer, 0, buffer.Length)) > 0)
+                            memstream.Write(buffer, 0, bytesRead);
+                        bytes = memstream.ToArray();
+                        if (TrackInfo.Image == bytes)
+                            return;
+                        TrackInfo.Image = bytes;
+                        bytes = null;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            GC.Collect();
+        }
+        private async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() => await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+        private async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session) => await session.TryGetMediaPropertiesAsync();
+        public void Dispose()
+        {
+            Disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+#pragma warning restore CA1416
+    public class TrackInfo
+    {
+        public string Track { get; set; }
+        public string Artist { get; set; }
+        public byte[] Image { get; set; } = Array.Empty<byte>();
+        public TrackInfo(string track, string artist)
+        {
+            Track = track;
+            Artist = artist;
+        }
+        public TrackInfo()
+        {
+            Track = "<track>";
+            Artist = "<artist>";
+        }
+        public override string ToString() => $"{Track}, {Artist}";
+    }
+}
---
+++ YandexMusicRPC/RPC.cs
@@ -0,0 +1,68 @@
+﻿using System.Data.SQLite;
+
+namespace YandexMusicRPC
+{
+    public static class RPC
+    {
+        public static DiscordRPC.DiscordRpcClient client { get; } = new("1096773597000912916");
+        public static bool Set(TrackInfo info)
+        {
+            try
+            {
+                string TrackImageUrl = "";
+                string? dbPath = null;
+                long TrackDuration = 0;
+                foreach (string dir in Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Packages")))
+                    if (dir.Contains("Yandex.Music"))
+                        foreach (string file in Directory.GetFiles(Path.Combine(dir, "LocalState")))
+                            if (Path.GetExtension(file)?.ToLower() == ".sqlite")
+                            {
+                                dbPath = Path.Combine(dir, "LocalState", Path.GetFileName(file));
+                                break;
+                            }
+                if (dbPath is null)
+                    return false;
+                SQLiteConnection yandexDBConnection = new($"Data Source={dbPath}");
+                yandexDBConnection.Open();
+                SQLiteCommand cmd = new($"SELECT * FROM T_Track WHERE Title=\"{info.Track}\"", yandexDBConnection);
+                SQLiteDataReader track = cmd.ExecuteReader();
+                if (track.Read())
+                {
+                    TrackImageUrl = "https://" + ((string)track["CoverUri"])[..^2] + "200x200";
+                    TrackDuration = (long)track["DurationMillis"];
+                }
+                yandexDBConnection.Close();
+                yandexDBConnection.Dispose();
+                client.SetPresence(new()
+                {
+                    Assets = new()
+                    {
+                        LargeImageKey = TrackImageUrl,
+                        LargeImageText = info.ToString(),
+                        SmallImageKey = "https://yastatic.net/s3/doc-binary/freeze/m0Q5UlZ5G_e_LHMDh2HJXl15mvM.png",
+                        SmallImageText = "Yandex Music"
+                    },
+                    Buttons = new DiscordRPC.Button[]
+                    {
+                        new()
+                        {
+                            Label = "Yandex Music",
+                            Url = "https://music.yandex.ru"
+                        }
+                    },
+                    Timestamps = new DiscordRPC.Timestamps()
+                    {
+                        End = DateTime.UtcNow.AddMilliseconds((ulong)TrackDuration)
+                    },
+                    State = "Listening",
+                    Details = info.ToString()
+                });
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
